1f97e545505fda904747c87e680ba281ceae6b15
wayo <dawayoapps@gmail.com>
2019-07-24 21:30:49 +0200

    added generated react native project

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,27 +1,92 @@
 import {Command, flags} from '@oclif/command'
+import * as inquirer from 'inquirer'
+import * as shell from 'shelljs'
+import cli from 'cli-ux'
+import * as signale from 'signale';
+import * as fs from 'fs';
+const replace = require('replace-in-file');
+
+export enum TYPE_OF_APP {
+  REACT_JS = 1,
+  REACT_NATIVE_JS = 2,
+  REACT_TS = 3,
+  REACT_NATIVE_TS = 4,
+  EXPO_TS = 5,
+}
 
 export default class Init extends Command {
-  static description = 'create a new project'
+  static flags = {
+    type: flags.string({options: ['React Native']}),
+    boilerplate: flags.string({options: ['React Native']}),
+    git: flags.boolean({default: false})
+  }
 
-  static args = [
-    {
-      name: 'file',               // name of arg to show in help and reference with args[name]
-      required: false,            // make the arg required with `required: true`
-      description: 'output file', // help description
-      hidden: true,               // hide this arg from help
-     // parse: input => 'output',   // instead of the user input, return a different value
-      default: 'world',           // default value if no arg input
-      options: ['a', 'b'],        // only allow input to be from a discrete set
-    }
-  ]
-  
   async run() {
-    // can get args as an object
-    const {args} = this.parse(Init)
-    console.log(`running my command with args: ${args.options}`)
-    // can also get the args as an array
-    const {argv} = this.parse(Init)
-    console.log(`running my command with args: ${argv[0]}, ${argv[1]}`)
+    const name = await cli.prompt('project name?')
+
+    const {flags} = this.parse(Init)
+    let type = flags.type
+    if (!type) {
+      let responses: any = await inquirer.prompt([{
+        name: 'type',
+        message: 'Select a type of project',
+        type: 'list',
+        choices: [{name: 'react-native'}],
+      }])
+      type = responses.type
+    }
+    this.log(`the stage is: ${type}`)
+    let boilerplate = flags.boilerplate
+    if (!boilerplate) {
+      let responses: any = await inquirer.prompt([{
+        name: 'boilerplate',
+        message: 'Select a boilerplate',
+        type: 'list',
+        choices: [{name: 'typescript-redux'}],
+      }])
+      boilerplate = responses.boilerplate
+    }
+    await this.clone(name,'none','none')
+    let useGit = flags.boilerplate
+    if (!boilerplate) {
+      let responses: any = await inquirer.prompt([{
+        name: 'boilerplate',
+        message: 'Select a boilerplate',
+        type: 'list',
+        choices: [{name: 'typescript-redux'}],
+      }])
+      boilerplate = responses.boilerplate
+    }
+  }
+
+  async clone(name: string, type: string, boilerplate: string) {
+    //cli.action.start('starting')
+    signale.await();
+    if (shell.exec('git clone https://github.com/microsoft/TypeScript-React-Native-Starter.git ./' + name, {silent:true}).code !== 0) {
+      signale.error('have git installed? or have same name folder?')
+      shell.exit(1);
+    } else {
+    //  cli.action.stop()
+      await this.packageJson('./lol/ExampleProject/', name)
+      signale.success();
+      
+    }
+  }
+
+  packageJson(file: any, projectName: string) {
+    const options = {
+      files: file + 'package.json',
+      from: /ExampleProject/g,
+      to: projectName,
+    };
+    try {
+      const results = replace.sync(options);
+    }
+    catch (error) {
+      signale.error('Error occurred:', error)
+      shell.exit(1);
+    }
   }
   
 }
+
